e1e95ed85375a1bae79c2d06da58cb7131307136
Marat Bryantcev <bryantsev.it11@mail.ru>
2017-12-06 00:18:13 +0300

    lab

---
+++ BookReading/Scripts/main.js
@@ -0,0 +1,42 @@
+﻿$('.like-btn').click(function () {
+    var reviewId = $(this).data('review');
+    var $this = $(this).children('span');
+    $.post({
+        'url': '/Review/IncrementAndGetLikes',
+        'data': { reviewId: reviewId },
+        'success': function (Like) {
+            $this.text('Лайкнуть: ' + Like);
+        },
+        'error': function (_, error) {
+            alert('error: ' + error)
+        }
+    });
+});
+
+$('.report-btn').click(function () {
+    var result = confirm("Вы хотите пожаловаться?");
+    var $this = $(this).parent();
+    if (result) {
+        var reviewId = $(this).data('review');
+        var reason = prompt('Введите причину жалобы:');
+        if (reason.trim() !== '') {
+            $.post({
+                'url': '/Review/ReportOffensiveReview',
+                'data': { reviewId: reviewId, reason: reason },
+                'success': function () {
+                    alert("Жалоба принята");
+                    $this.css('opacity', '.7');
+                },
+                'error': function (_, error) {
+                    alert('error: ' + error)
+                }
+            });
+        }
+        else {
+            return false;
+        }
+    }
+    else {
+        return false;
+    }
+});
